d2560e997462d048c5a5be5c6a805477ba6d6688
ozbannot <ozbannot@gmail.com>
2022-11-27 14:08:01 +0900

    crate user model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # bcrypt
+  has_secure_password
+
+  validates :name, presence: true,
+                   length: {
+                     maximum: 30,
+                     allow_blank: true
+                   }
+  
+  VALID_PASSWORD_REGEX = /\A[w\-]+\z/
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX
+                       },
+                       allow_nil: true
+end
