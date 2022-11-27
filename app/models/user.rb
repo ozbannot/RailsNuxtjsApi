849a36bb15f25196c6196b2f18224e69a8f3a528
ozbannot <ozbannot@gmail.com>
2022-11-27 15:17:15 +0900

    add lib/validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
+require "validator/email_validator"
 class User < ApplicationRecord
+  before_validation :downcase_email
   # bcrypt
   has_secure_password
 
@@ -7,13 +9,31 @@ class User < ApplicationRecord
                      maximum: 30,
                      allow_blank: true
                    }
-  
-  VALID_PASSWORD_REGEX = /\A[w\-]+\z/
+
+  validates :email, presence: true,
+                    email: { allow_blank: true }
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
                        length: { minimum: 8 },
                        format: {
                          with: VALID_PASSWORD_REGEX,
                          message: :invalid_password
                        },
-                       allow_nil: true
+                       allow_blank: true
+
+  class << self
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_activated(email).present?
+  end
+
+  private
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
